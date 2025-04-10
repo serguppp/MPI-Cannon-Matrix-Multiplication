e949d0c796c94a531e6d4fba34b8cb5c00d8b23e
serguppp <serg.u@wp.pl>
2025-04-10 21:56:13 +0200

    Update gitignore, added relative paths to generator.c

--- generator.c
+++ generator.c
@@ -5,21 +5,34 @@ int main(int argc, char **argv) {
     FILE *plik, *plik2;
     int i, j;
 
-    plik = fopen("C:/Users/sergi/Desktop/mpi_projekt/matrix1.txt", "w");
+    // Określenie ścieżki w zależności od systemu operacyjnego
+    // Info: ze względu na bug kompilatora nie mogę zrobić ścieżki relatywnej, więc póki co jest tylko dla mnie
+    #ifdef _WIN32  // Windows
+        const char *path1 = "C:/Users/sergi/Desktop/mpi_projekt/matrix1.txt";
+        const char *path2 = "C:/Users/sergi/Desktop/mpi_projekt/matrix2.txt";
+    #else  // Linux
+        const char *path1 = "./matrix1.txt";
+        const char *path2 = "./matrix2.txt";
+    #endif
+
+    // Otwieranie plików
+    plik = fopen(path1, "w");
     if (plik == NULL) {
         perror("Błąd otwarcia pliku");
         exit(EXIT_FAILURE);
     }
 
-    plik2 = fopen("C:/Users/sergi/Desktop/mpi_projekt/matrix2.txt", "w");
+    plik2 = fopen(path2, "w");
     if (plik2 == NULL) {
         perror("Błąd otwarcia pliku");
         exit(EXIT_FAILURE);
     }
-    printf("Pliki otwarte poprawnie, zapisuje dane\n");
 
+    printf("Pliki otwarte poprawnie, zapisuję dane\n");
+
+    // Zapis danych do plików
     for (i = 0; i < 2000; i++) {
-        for (j = 0; j < 2000; j++){
+        for (j = 0; j < 2000; j++) {
             fprintf(plik, "%10.2lf ", (double)i + j);
             fprintf(plik2, "%10.2lf ", (double)i + j);
         }
@@ -27,9 +40,10 @@ int main(int argc, char **argv) {
         fprintf(plik2, "\n");
     }
 
+    // Zamknięcie plików
     fclose(plik);
     fclose(plik2);
-    printf("Pliki został zapisane i zamknięte poprawnie\n");
+    printf("Pliki zostały zapisane i zamknięte poprawnie\n");
 
     return 0;
 }
